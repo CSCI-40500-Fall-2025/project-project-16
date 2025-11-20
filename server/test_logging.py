f2e1770e328b37835485db754af029d138ab767c
Sudiptto Biswas <biswassudiptto@gmail.com>
2025-11-19 22:23:19 -0500

    testing

---
+++ server/test_logging.py
@@ -0,0 +1,7 @@
+from server.deezer import get_random_artists
+import logging
+
+def test_logging():
+    # This will produce logs inside CI
+    get_random_artists(2)
+    logging.info("Test completed")
